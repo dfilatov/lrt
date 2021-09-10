9ad4f432d236b3f513f0dddaaa1bc4b20f95a16b
Filatov Dmitry <dfilatov@yandex-team.ru>
2021-09-10 17:36:34 +0300

    Bump deps

--- src/chunkSchedulers/idleCallback.ts
+++ src/chunkSchedulers/idleCallback.ts
@@ -13,23 +13,13 @@ interface RequestIdleCallback {
     (deadline: RequestIdleCallbackDeadline): void;
 }
 
-declare global {
-    interface Window {
-        requestIdleCallback?(
-            callback: RequestIdleCallback,
-            options?: RequestIdleCallbackOptions
-        ): number;
-        cancelIdleCallback?(handle: number): void;
-    }
-}
-
 const idleCallbackChunkScheduler: ChunkScheduler<number> | null =
     typeof window !== 'undefined' &&
     typeof window.requestIdleCallback === 'function' &&
     typeof window.cancelIdleCallback === 'function' ?
         {
-            request: fn => window.requestIdleCallback!(fn),
-            cancel: token => window.cancelIdleCallback!(token)
+            request: fn => window.requestIdleCallback(fn),
+            cancel: token => window.cancelIdleCallback(token)
         } :
         null;
 
